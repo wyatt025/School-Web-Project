b0adb7d96c7538900c99f5f3e11f06997c10b40e
Jagrithanda <jagrithanda2011@gmail.com>
2026-03-26 21:10:36 +0530

    Header of the website

--- frontEndJS/index.js
+++ frontEndJS/index.js
@@ -1,1 +1,13 @@
+function toggleMenu() {
+    document.getElementById("dropdown-menu").classList.toggle("show");
+}
 
+// Close the menu if the user clicks anywhere else on the screen
+window.onclick = function(event) {
+  if (!event.target.closest('.create-container')) {
+    var dropdowns = document.getElementsByClassName("create-dropdown");
+    for (var i = 0; i < dropdowns.length; i++) {
+      dropdowns[i].classList.remove('show');
+    }
+  }
+}
