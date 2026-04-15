25e23abd43308d971a400061d6148601ed5a7ba1
Chalith <chalith92@gmail.com>
2026-04-15 11:38:30 +0300

    Implement user registration flow

--- js/signup.js
+++ js/signup.js
@@ -1,3 +1,33 @@
-function onBackClick(){
+function openLoginPage(){
     window.location.href='../html/login.html'
 }
+
+document.getElementById("signupForm").addEventListener("submit", async function(e) {
+    e.preventDefault();
+
+    const data = {
+        firstName: document.getElementById("firstName").value,
+        lastName: document.getElementById("lastName").value,
+        dob: document.getElementById("dob").value,
+        gender: document.getElementById("gender").value,
+        email: document.getElementById("email").value,
+        password: document.getElementById("password").value
+    };
+
+    try {
+        const response = await fetch("http://localhost:3001/register", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        });
+
+        const result = await response.json();
+        alert(result.message);
+        openLoginPage()
+
+    } catch (error) {
+        alert("Error connecting to server");
+    }
+});
